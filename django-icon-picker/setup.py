097c9f2564b9fa932dae308d47737c3f2bfa4170
Ryan Malloy <rpm@malloys.us>
2025-06-09 20:11:17 -0600

    Set minimum Python version to 3.9 and update classifiers

--- django-icon-picker/setup.py
+++ django-icon-picker/setup.py
@@ -16,6 +16,7 @@ setup(
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
+    python_requires=">=3.9",
     install_requires=[
         "Django>=3.0",
         "requests",
@@ -33,9 +34,9 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
 )
